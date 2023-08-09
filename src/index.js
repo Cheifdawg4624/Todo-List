700daf500dbe3f13c4e75f02aed08697786860b2
Cheifdawg4624 <rmaher4624@gmail.com>
2023-08-09 13:55:31 -0400

    added button and form to submit new todo

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import _ from "lodash";
 import "./style.css";
+const todoBtn = document.querySelector(".add");
+const modal = document.getElementById("myModal");
+const span = document.querySelector(".close");
+todoBtn.addEventListener("click", function () {
+  modal.style.display = "block";
+});
 
-function component() {
-  const element = document.createElement("div");
+span.addEventListener("click", function () {
+  modal.style.display = "none";
+});
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
-  element.classList.add("hello");
-  return element;
-}
+window.addEventListener("click", function (e) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+});
+function component() {}
 
-document.body.appendChild(component());
+// document.body.appendChild(component());
